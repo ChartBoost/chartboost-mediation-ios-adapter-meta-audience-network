20d499ad11a0327263e5b158e7ee159573b4d61d
Daniel Barros <daniel.barros@chartboost.com>
2022-11-23 15:30:15 +0100

    Update version string requirements

--- Source/MetaAudienceNetworkAdapter.swift
+++ Source/MetaAudienceNetworkAdapter.swift
@@ -17,9 +17,9 @@ final class MetaAudienceNetworkAdapter: PartnerAdapter {
     let partnerSDKVersion = FB_AD_SDK_VERSION
     
     /// The version of the adapter.
-    /// It should have 6 digits separated by periods, where the first digit is Helium SDK's major version, the last digit is the adapter's build version, and intermediate digits are the partner SDK's version.
-    /// Format: `"<Helium major version>.<Partner major version>.<Partner minor version>.<Partner patch version>.<Partner build version>.<Adapter build version>"`.
-    let adapterVersion = "4.6.9.0.0.0"
+    /// It should have either 5 or 6 digits separated by periods, where the first digit is Helium SDK's major version, the last digit is the adapter's build version, and intermediate digits are the partner SDK's version.
+    /// Format: `<Helium major version>.<Partner major version>.<Partner minor version>.<Partner patch version>.<Partner build version>.<Adapter build version>` where `.<Partner build version>` is optional.
+    let adapterVersion = "4.6.9.0.0"
     
     /// The partner's unique identifier.
     let partnerIdentifier = "facebook"
