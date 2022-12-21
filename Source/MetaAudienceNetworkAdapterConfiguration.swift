e2cb7e993f5b4ef1790ba82c54d2d56bc9a9fd74
Daniel Barros <daniel.barros@chartboost.com>
2022-12-21 15:57:30 +0100

    Make configuration class compatible with Obj-C

--- Source/MetaAudienceNetworkAdapterConfiguration.swift
+++ Source/MetaAudienceNetworkAdapterConfiguration.swift
@@ -9,44 +9,27 @@ import Foundation
 import FBAudienceNetwork
 
 /// A list of externally configurable properties pertaining to the partner SDK that can be retrieved and set by publishers.
-public class MetaAudienceNetworkAdapterConfiguration {
+@objc public class MetaAudienceNetworkAdapterConfiguration: NSObject {
+    
     /// Flag that can optionally be set to enable Meta Audience Network's test mode. Make sure to disable test mode in production.
     /// Disabled by default.
-    private static var _testMode = false
-    public static var testMode: Bool {
-        get {
-            return _testMode
-        }
-        set {
-            _testMode = newValue
-            
-            if (_testMode) {
+    @objc public static var testMode: Bool = false {
+        didSet {
+            if testMode {
                 FBAdSettings.addTestDevice(FBAdSettings.testDeviceHash())
             } else {
                 FBAdSettings.clearTestDevices()
             }
-            
-            print("The Meta Audience Network SDK's test mode is \(_testMode ? "enabled" : "disabled").")
+            print("Meta Audience Network SDK test mode set to \(testMode)")
         }
     }
     
     /// Flag that can optionally be set to enable Meta Audience Network's verbose logging.
     /// Disabled by default.
-    private static var _verboseLogging = false
-    public static var verboseLogging: Bool {
-        get {
-            return _verboseLogging
-        }
-        set {
-            _verboseLogging = newValue
-            
-            if (_verboseLogging) {
-                FBAdSettings.setLogLevel(FBAdLogLevel.verbose)
-            } else {
-                FBAdSettings.setLogLevel(FBAdLogLevel.log)
-            }
-            
-            print("The Meta Audience Network SDK's verbose logging is \(_verboseLogging ? "enabled" : "disabled").")
+    @objc public static var verboseLogging: Bool = false {
+        didSet {
+            FBAdSettings.setLogLevel(verboseLogging ? .verbose : .log)
+            print("Meta Audience Network SDK verbose logging set to \(verboseLogging)")
         }
     }
 }
